acc1502788abf1d3b6d535e98b84667b8acfb64b
Maksim <mysik.bazuk@gmail.com>
2022-02-17 22:47:07 +0200

    delete field with tests

--- src/test/java/collections/MyMapTest.java
+++ src/test/java/collections/MyMapTest.java
@@ -26,7 +26,6 @@ class MyMapTest {
         MyMap<WeekDay, Integer> myMap = new MyMap<>();
         myMap.put(WeekDay.MONDAY, 0);
         myMap.put(WeekDay.TUESDAY, 1);
-        myMap.put(WeekDay.TUESDAY, 1);
         myMap.put(WeekDay.WEDNESDAY, 2);
         myMap.put(WeekDay.THURSDAY, 3);
         myMap.put(WeekDay.FRIDAY, 4);
